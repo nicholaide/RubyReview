ec5d69c94c1170033f0dff79674eb29ea9c3f7f7
Nicholai de Guzman <ngdeguzman@hotmail.com>
2012-06-03 18:55:21 -0500

    B4 iter F5

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -3,5 +3,7 @@ class StoreController < ApplicationController
   def index
 	@products = Product.order(:title)
 	increment_counter
+
+        @cart = current_cart
   end
 end
--- depot/app/controllers/store_controller.rb~
+++ depot/app/controllers/store_controller.rb~
@@ -2,5 +2,6 @@
 class StoreController < ApplicationController
   def index
 	@products = Product.order(:title)
+	increment_counter
   end
 end
