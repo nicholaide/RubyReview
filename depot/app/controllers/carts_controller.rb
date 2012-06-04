fa5fdf0d000ef136344ad8f29ef3f0cc4924dd34
Nicholai de Guzman <ngdeguzman@hotmail.com>
2012-06-03 23:23:42 -0500

    after Task F play task 1

--- depot/app/controllers/carts_controller.rb
+++ depot/app/controllers/carts_controller.rb
@@ -90,6 +90,7 @@ class CartsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to store_url }
+      format.js { }
       format.json { head :no_content }
     end
   end
