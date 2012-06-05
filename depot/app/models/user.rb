cfc8b01ece4ee0a5d3744210f6487b28f28cd6b8
Nicholai de Guzman <ngdeguzman@hotmail.com>
2012-06-04 23:42:03 -0500

    before iter I playtime

--- depot/app/models/user.rb
+++ depot/app/models/user.rb
@@ -4,5 +4,15 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  #called after the SQL delete is executed
+  after_destroy :ensure_an_admin_remains
+
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        raise "Can't delete last user"
+      end
+    end   
      
 end
